e044d7f73a05612e5f10fa1b1966496c49395072
Robert Kerr <robkerr87@gmail.com>
2013-10-04 11:28:19 +1000

    Recursive method for creating a nested array representing the tournament bracket from a list of contenders ordered by rank/seed.

---
+++ lisa/create_bracket.rb
@@ -0,0 +1,20 @@
+Bundler.require
+
+def partition_seeds seeds
+	return seeds if seeds.length <= 2
+	seeds_with_indices = seeds.each_with_index 
+	partitioned_seeds_with_indices = seeds_with_indices.partition { |v,i| (i%4)==1 || (i%4)==2 }
+	partitioned_seeds = partitioned_seeds_with_indices.map { |e| e.map { |v,i| v }}
+	partitioned_seeds.map { |s| partition_seeds(s) }
+end
+
+all_episodes = begin
+	full_hash = JSON.parse(File.read("data/episodes.json"))
+	full_hash.map { |ep| "#{ep['season']}.#{ep['episode']} #{ep['title']}" }
+end
+
+episodes = all_episodes[0..511]
+bracket = partition_seeds(episodes)
+test_bracket = partition_seeds((0..16).to_a)
+
+binding.pry
